9e9e05beb363a66523f20b568acb9c992c667be5
Kellen Murphy <kellen@bcsengineering.com>
2022-07-15 15:03:15 -0400

    main wrapper

--- wrap.sh
+++ wrap.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 # Wrap shell script from @iay/shibboleth-idp-docker with our VERSIONS and execute
 
+files=("VERSIONS" "install-idp") 
 mkdir ./tmp
-cp ./shibboleth-idp-docker/VERSIONS ./tmp/VERSIONS-ORIG
-cp ./VERSIONS ./shibboleth-idp-docker/VERSIONS
+for file in ${files[@]}; do
+    cp ./shibboleth-idp-docker/$file ./tmp/$file-ORIG
+    cp ./$file ./shibboleth-idp-docker/$file
+done
+
+
 echo "executing: $1 from @iay/shibboleth-idp-docker..."
 if test -f "./shibboleth-idp-docker/$1"; then
-    ./shibboleth-idp-docker/$1
+    cd ./shibboleth-idp-docker/
+    ./$1
+    cd ..
 fi
-cp ./tmp/VERSIONS-ORIG ./shibboleth-idp-docker/VERSIONS
+
+for file in ${files[@]}; do
+    cp ./tmp/$file-ORIG ./shibboleth-idp-docker/$file
+done
 rm -rf ./tmp
