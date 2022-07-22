4828b2e087e6c9e09ddcb66f3be9de6c18c628fa
Kellen Murphy <kellen@bcsengineering.com>
2022-07-22 09:59:05 -0400

    real cred check for userfacing cert

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 if [ -e './shibboleth-idp' ]; then
     cp -r ./shibboleth-idp/* ./shibboleth-idp-docker/shibboleth-idp
 fi
-if [ ! -e './shibboleth-idp/credentials' ]; then
+if [ ! -e './shibboleth-idp/credentials/idp-userfacing.p12' ]; then
     ./wrap.sh gen-selfsigned-cert
 fi
 ./wrap.sh build
