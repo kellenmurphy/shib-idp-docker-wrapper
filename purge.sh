258b3064bdd5f5254e69299b69f03fed586e1cf6
Kellen Murphy <kellen@bcsengineering.com>
2022-07-27 16:35:00 -0400

    update to purge.sh

--- purge.sh
+++ purge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # clean up downloaded files and purge images
-
+./wrap.sh terminate
 files=("fetched-*" "fetched" "jetty-dist" "shibboleth-idp" "logs") 
 for file in ${files[@]}; do
     rm -rf ./shibboleth-idp-docker/$file
--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./purge.sh 
-./wrap.sh terminate
 ./wrap.sh fetch-jetty
 ./wrap.sh fetch-shib
 ./wrap.sh install
