fc17478f9dc53ec837fce86f0ac301c262bd2dd8
Kellen Murphy <kellen@bcsengineering.com>
2022-07-28 12:25:21 -0400

    refactoring for overlay-directory

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -e './shibboleth-idp' ]; then
-    cp -r ./shibboleth-idp/* ./shibboleth-idp-docker/shibboleth-idp
+if [ -e './idp-overlay/shibboleth-idp' ]; then
+    cp -r ./idp-overlay/shibboleth-idp/* ./shibboleth-idp-docker/shibboleth-idp
 fi
-if [ ! -e './shibboleth-idp/credentials/idp-userfacing.p12' ]; then
+if [ ! -e './idp-overlay/shibboleth-idp/credentials/idp-userfacing.p12' ]; then
     ./wrap.sh gen-selfsigned-cert
 fi
 ./wrap.sh build
